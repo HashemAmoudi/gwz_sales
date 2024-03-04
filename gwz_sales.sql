ccdbbdedb996f7ced587c1bc9440f6ff77a65d46
HashemAmoudi <160497119+HashemAmoudi@users.noreply.github.com>
2024-03-04 13:42:53 +0300

    rounding_result
    describe rounding results

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -8,7 +8,7 @@ GROUP BY
 ORDER BY
   date;
   SELECT date_date,
-ROUND (SUM(turnover),1) AS turnover,
-ROUND (SUM(purchase_cost),1) AS purchase_cost
+ROUND (SUM(turnover),2) AS turnover,
+ROUND (SUM(purchase_cost),2) AS purchase_cost
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales`
 GROUP BY date_date
